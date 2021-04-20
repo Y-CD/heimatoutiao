c6500e6ffe3e03ca805e41e90f4aabbb6f515226
Ychunda <2468316840@qq.com>
2021-04-20 09:09:11 +0800

    项目的初始结构搭建完成

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,12 @@
 <template>
-  <div id="app"></div>
+  <div id="app">项目开始了</div>
 </template>
 
 <script>
 export default {
   name: "App",
-  components: {},
 };
 </script>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入全局的重置样式
+import '@/styles/reset.less'
+
 Vue.config.productionTip = false
 
 new Vue({
